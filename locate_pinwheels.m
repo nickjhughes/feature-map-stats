625511b034d1517d1fa065bfded5513b51136f8a
nickjhughes <nickjhughes@gmail.com>
2016-04-05 20:15:27 +1000

    Added pinwheel functions and updated licence.

---
+++ locate_pinwheels.m
@@ -0,0 +1,108 @@
+
+function [pinw, windno] = locate_pinwheels(map)
+%LOCATE_PINWHEELS Locate pinwheels in an OP map.
+%
+% [pinw, windno] = locate_pinwheels(map)
+%
+% Finds the location and winding number of all pinwheels in the given OP map
+% in complex form.
+% 
+% Notes:
+%
+% - The sign of a pinwheel is defined positive if the orientation increases
+%   clockwise and negative if anticlockwise. This seems to be the adopted
+%   convenion in neuroscience, but unfortunately it is the opposite of the
+%   one used in physics!
+%
+% - The positive (pinwp) and negative (pinwn) pinwheel locations can be
+%   obtained as follows:
+%     pinwp = pinw(windno > 0,:);
+%     pinwn = pinw(windno < 0,:);
+%   In principle, the winding number should be either 1 or -1 for all
+%   pinwheels (and 0 for non-pinwheel points), but it might happen that it
+%   is ±2 or higher for a pinwheel. I don't think that has ever happened to
+%   anyone with OR maps, but keep an eye on it!
+%
+% Copyright (c) 2002 by Miguel A. Carreira-Perpinan
+
+% Contour integral of radius one around each pixel (except the border pixels)
+angles = mod(angle(map), 2*pi);
+pinwq = nan(size(map));
+for j = 2:size(map,1)-1
+    for k = 2:size(map,2)-1
+        indicies = [j-1,k-1; j-1,k; j-1,k+1; j,k+1; ...
+                    j+1,k+1; j+1,k; j+1,k-1; j,k-1; ...
+                    j-1,k-1];
+        L = zeros(length(indicies),1);
+        for p = 1:length(indicies)
+            L(p) = angles(indicies(p,1),indicies(p,2));
+        end
+        if any(isnan(L))
+            pinwq(j,k) = 0;
+        else
+            pinwq(j,k) = sum(diff(my_unwrap(L)));
+        end
+    end
+end
+pinwq = abs(pinwq) > 0.1;
+
+% Join clusters of pinwheels and reduce to their centre of mass
+[tmp, np] = bwlabel(pinwq, 8);
+tmp = regionprops(tmp, 'Centroid');
+pinw = reshape([tmp.Centroid], 2, np)';
+
+% Calculate winding numbers
+if nargout == 2
+    if isempty(pinw)
+        windno = [];
+        return;
+    end
+    windno = zeros(length(pinw),1);
+    for l = 1:size(pinw,1)
+        j = round(pinw(l,2));
+        k = round(pinw(l,1));
+        indicies = [j-1,k-1; j-1,k; j-1,k+1; j,k+1; ...
+                    j+1,k+1; j+1,k; j+1,k-1; j,k-1; ...
+                    j-1,k-1];
+        L = zeros(length(indicies),1);
+        for p = 1:length(indicies)
+            L(p) = angles(indicies(p,1),indicies(p,2));
+        end
+        windno(l) = sum(diff(unwrap(L)))/(2*pi);
+    end
+end
+
+% Check for some possible errors
+if any(abs(round(windno)-windno) > 10^(-5))
+    warning('Some winding numbers are not exactly integers.');
+end
+if any(abs(windno)-1>eps*10)
+    warning('Some winding numbers are different from ±1.');
+end
+
+
+function q = my_unwrap(p)
+% A simplified version of unwrap because p is always a column vector here.
+
+% Unwrap each column of p
+q = p;
+% Find NaN's and Inf's
+indf = find(isfinite(p(:)));
+% Unwrap finite data (skip non finite entries)
+q(indf) = LocalUnwrap(p(indf));
+
+function p = LocalUnwrap(p)
+%LocalUnwrap   Unwraps column vector of phase values.
+
+m = length(p);
+
+% Unwrap phase angles.  Algorithm minimizes the incremental phase variation 
+% by constraining it to the range [-pi,pi]
+dp = diff(p,1,1);                % Incremental phase variations
+dps = mod(dp+pi,2*pi) - pi;      % Equivalent phase variations in [-pi,pi)
+dps(dps==-pi & dp>0,:) = pi;     % Preserve variation sign for pi vs. -pi
+dp_corr = dps - dp;              % Incremental phase corrections
+dp_corr(abs(dp)<pi,:) = 0;       % Ignore correction when incr. variation is < CUTOFF
+
+% Integrate corrections and add to P to produce smoothed phase values
+p(2:m,:) = p(2:m,:) + cumsum(dp_corr,1);
---
+++ pinwod.m
@@ -0,0 +1,100 @@
+
+function [pinwod_dist, counts] = pinwod(op, od, mask, nbins, rpinw)
+%PINWOD Calculate histogram of pinwheel/OD spatial relationship.
+%
+% [pinwod_dist, counts] = pinwod(op, od, mask, nbins, rpinw)
+%
+% Calculates a distribution of OP pinwheel locations relative to an OD map.
+% The positive and negative parts of the OD map are split into nbins (5 by
+% default) bins such that each contains the same number of pixels. Each OP
+% pinwheel is assigned to the appropriate bin, and the positive and negative
+% bins are combined. A binary mask can be provided to restrict the analysis.
+% If repeatedly analysing a pair of maps, pinwheel location can be pre-computed
+% and provided as rpinw for efficiency. The second output contains the raw
+% pinwheel counts in each bin.
+%
+% This metric is a modification of the metric used in:
+% Hubener M, Shoham D, Grinvald A, Bonhoeffer T (1997) "Spatial relationships
+% among three columnar systems." J Neurosci 17(23):9270-9284.
+
+% Input defaults and validation
+if nargin < 2
+    error('At least two inputs are required.');
+end
+if nargin < 3
+    mask = true(size(op));
+end
+if nargin < 4
+    nbins = 5;
+end
+if nargin < 5
+    rpinw = [];
+end
+
+% Mask
+op(~mask) = nan;
+od(~mask) = nan;
+
+% Split the OD map into positive and negative sections
+od_pos = od;
+od_pos(od <= 0) = nan;
+od_neg = od;
+od_neg(od > 0) = nan;
+
+% Divide the two OD map parts into nbins quantiles
+% Positive
+if nbins == 2
+    q = quantile(od_pos(:), 2);
+    q = q(1);
+else
+    q = quantile(od_pos(:), nbins-1);
+end
+bin_od_pos = nan(size(od_pos));
+bin_od_pos(od_pos < q(1)) = nbins;
+for k = 2:nbins-1
+    bin_od_pos(od_pos < q(k) & od_pos > q(k-1)) = nbins - k + 1;
+end
+bin_od_pos(od_pos > q(nbins-1)) = 1;
+% Negative
+if nbins == 2
+    q = quantile(od_neg(:), 2);
+    q = q(1);
+else
+    q = quantile(od_neg(:), nbins-1);
+end
+bin_od_neg = nan(size(od_neg));
+bin_od_neg(od_neg < q(1)) = nbins;
+for k = 2:nbins-1
+    bin_od_neg(od_neg < q(k) & od_neg > q(k-1)) = nbins - k + 1;
+end
+bin_od_neg(od_neg > q(nbins-1)) = 1;
+
+% Find pinwheels in OP
+if ~isempty(rpinw)
+    rpinw = locate_pinwheels(op);
+end
+npinw = size(rpinw, 1);
+
+% Calculate histogram of pinwheels in OD quantiles
+pinwod_dist = zeros(2, nbins);
+for l = 1:npinw
+    % Determine if its in the negative or positive section
+    if isnan(bin_od_pos(round(rpinw(l,2)), round(rpinw(l,1))))
+        % Negative
+        p = bin_od_neg(round(rpinw(l,2)), round(rpinw(l,1)));
+        if ~isnan(p)
+            pinwod_dist(2,p) = pinwod_dist(2,p) + 1;
+        end
+    else
+        % Positive
+        p = bin_od_pos(round(rpinw(l,2)), round(rpinw(l,1)));
+        if ~isnan(p)
+            pinwod_dist(1,p) = pinwod_dist(1,p) + 1;
+        end
+    end
+end
+
+% Combine the two histograms
+pinwod_dist = pinwod_dist(1,:) + fliplr(pinwod_dist(2,:));
+counts = pinwod_dist;
+pinwod_dist = pinwod_dist/sum(pinwod_dist);
